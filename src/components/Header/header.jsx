cb1ca24915f82abc40c204aa88f828216c66e695
FeyzaO-H04 <feyzaoh04.kodehode@gmail.com>
2023-10-22 23:22:32 +0200

    created counter component

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,23 +1,11 @@
-import { useState } from "react"
+import { Filter } from "../Filter/filter"
 import "./header.css"
 import {
   Link,
 } from 'react-router-dom'
-import { Card } from "../Main/card"
-
 
 
 export function Header(){
-  /*
-  const clothes = Card ();
-  const [filter, setFilter] = useState("");
-  const filteredClothes = clothes.filter((item) => item.tittel.includes(filter));
-
-  function updateFilter(event) {
-    setFilter(event.target.value)
-  }
-
-  console.log(filter)  */
   
   return(
     <div className="header">
@@ -28,10 +16,7 @@ export function Header(){
             <li><Link to="/man">MAN</Link></li>
           </ul>
 
-          <div className="searchContainer">
-            <input className="searchInput" type="text" placeholder="Search" /* onChange={updateFilter} *//>
-            <button className="searchButton"><i className="fa-solid fa-magnifying-glass"></i></button>
-          </div>
+          <Filter />
 
         </div>
         <Link to="/" className="logo">LAKOS</Link>
