c99d4553925c08a0a3186951a8b69dab5a83a429
FeyzaO-H04 <feyzaoh04.kodehode@gmail.com>
2023-10-22 21:44:27 +0200

    created count and filter

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,21 +1,40 @@
+import { useState } from "react"
 import "./header.css"
 import {
   Link,
 } from 'react-router-dom'
+import { Card } from "../Main/card";
 
 
 
 export function Header(){
+  const clothes = Card ();
+  const [filter, setFilter] = useState("");
+  const filteredClothes = clothes.filter((item) => item.tittel.includes(filter));
+
+  function updateFilter(event) {
+    setFilter(event.target.value)
+  }
+
+  console.log(filter)
+  
   return(
     <div className="header">
       <div className="nav-bar"> 
-        <ul className="link-container">
-          <li><Link to="/woman">WOMAN</Link></li>
-          <li><Link to="/man">MAN</Link></li>
-        </ul>
+        <div className="nav-bar-left">
+          <ul className="link-container">
+            <li><Link to="/woman">WOMAN</Link></li>
+            <li><Link to="/man">MAN</Link></li>
+          </ul>
+
+          <div className="searchContainer">
+            <input className="searchInput" type="text" placeholder="Search" onChange={updateFilter}/>
+            <button className="searchButton"><i className="fa-solid fa-magnifying-glass"></i></button>
+          </div>
+
+        </div>
         <Link to="/" className="logo">LAKOS</Link>
         <ul className="link-container">
-          <li><a href="http://" target="_blank" rel="noopener noreferrer">SEARCH</a></li>
           <li><a href="http://" target="_blank" rel="noopener noreferrer">SIGN IN</a></li>
           <li><a href="http://" target="_blank" rel="noopener noreferrer">FAVORITES</a></li>
           <li><a href="http://" target="_blank" rel="noopener noreferrer">SHOPPING BAG</a></li>
--- src/components/Main/card.jsx
+++ src/components/Main/card.jsx
@@ -1,13 +1,33 @@
-
+import { useState } from "react"
 import "./card.css"
 
-export function Card(props){
+export function Card(props) {
+  const [count, setCount] = useState(0)
+
+  function increment() {
+    setCount((oldCount) => oldCount + 1)
+  }
+
+  function subtraction() {
+    setCount((oldCount) => oldCount - 1)
+  }
+
+  function reset() {
+    setCount(0)
+  }
+
   return(
     <div className="clothes-card">
       <a href={props.url} target="_blank" rel="noopener noreferrer"><img className = "clothes-image" src={props.img} alt={props.tittel}/></a>
       <a href={props.url} target="_blank" rel="noopener noreferrer"><h3>{props.tittel}</h3></a>
       <p>{props.price}</p>
-    
+
+      <div className="shoppingBag">
+        <button className="subtractionButton" onClick={subtraction}>-</button>
+        <div className="count">{count}</div>
+        <button className="incrementButton" onClick={increment}>+</button>
+        <button className="resetButton" onClick={reset}>RESET</button>
+      </div>
     </div>
   )
 }
